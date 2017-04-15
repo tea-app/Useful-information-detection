fdaf53f5a25b284925203aba72a4631999a28b63
panzou25 <1998panzou@gmail.com>
2017-04-15 23:49:03 +0900

    Correction

--- addComment.php
+++ addComment.php
@@ -6,11 +6,11 @@ require_once('./databaseComment.php');
 
 $db = new DatabaseComment();
 
-$article_id = 1; //記事ごとのID追加
+$article_id = $_GET['article_id'];//記事ごとのID追加
 $comment = $_POST['comment'];
 
 $db->addComment($article_id, $comment);
 
-header('Location: article.php');
+header('Location: index.php');
 exit;
 ?>
