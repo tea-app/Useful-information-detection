b1e612cb0c1d98fbb5774d89ccc19052cdf6be05
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2017-04-19 13:13:06 +0900

    no completed

--- index.php
+++ index.php
@@ -4,145 +4,108 @@ require_once('./Database.php');
 $db = new Database();
 
 $all = $db->Acquisition();
+
 ?>
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang = "ja">
     <head>
-        <meta charset="utf-8">
-        <title>glasoku</title>
-        <link href="assets/css/reset.css" type="text/css" rel="stylesheet">
+        <meta charset = "utf-8">
+        <title>グラ速</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link href="assets/css/reset.css" type="text/css" rel="stylesheet">
+        <!--index.cssのリンクタブ-->
         <link href="assets/css/index.css" type="text/css" rel="stylesheet">
     </head>
     <body>
+        <!--ここからヘッダー-->
         <header>
-            <div class=container>
-                <div class=header-list>
-                    <a href="index.php">トップページ</a>
-                </div>
-                <div class=header-list>
-                    <a>政治</a>
-                </div>
-                <div class=header-list>
-                    <a>小説</a>
-                </div>
-                <div class=header-list>
-                    <a>エンタメ</a>
-                </div>
-                <div class=header-list>
-                    <a>アニメ・漫画</a>
-                </div>
-                <div class=header-list>
-                    <a>ゲーム</a>
-                </div>
-                <div class=header-list>
-                    <a>IT</a>
-                </div>
-                <div class=header-list>
-                    <a>スポーツ</a>
-                </div>
-                <div class=header-list>
-                    <a href="http://kemono-friends.jp">ふれんず</a>
-                </div>
-                <div class=header-list>
-                    <a>その他</a>
-                </div>
+            <div class = "container">
+                <div class = "header-list selected"> <a href="index.php">トップページ</a> </div>
+                <div class = "header-list"> <a>政治</a> </div>
+                <div class = "header-list"> <a>小説</a> </div>
+                <div class = "header-list"> <a>エンタメ</a> </div>
+                <div class = "header-list"> <a>アニメ・漫画</a> </div>
+                <div class = "header-list"> <a>ゲーム</a> </div>
+                <div class = "header-list"> <a>IT</a> </div>
+                <div class = "header-list"> <a>スポーツ</a> </div>
+                <div class = "header-list"> <a>フレンズ</a> </div>
+                <div class = "header-list"> <a>その他</a> </div>
             </div>
         </header>
-        <div class=header-title>
-            <p>Scientific Glasses 速報</p>
-        </div>
-        <div class=titles>
-            <div class=title-box>
-                <a href="indexSub.php">睡眠は必要か不要か</a>
-                
+        <!--ここからタイトル-->
+        <div class = "top">
+            <div class = "title-box">
+                <p>Scientific Glasses 速報</p> 
             </div>
         </div>
-        <div class=sidebar>
-            <div class=sidebar-left>
-                <div class=sidebar-box>
-                    <p>　　　</p>
-                </div>
-            </div>
-            <div class=sidebar-right>
-                <div class=sidebar-box>
-                </div>
-            </div>
-        </div>
-        <div class=design>
-            <div class=main-bar>
-                <p>＜人気記事まとめ＞</p>
-            </div>
-            <div class=side-tagFirst>
-                <p>＜PR＞</p>
-            </div>
-            <div class=side-tagSecond>
-                <p>＜PR＞</p>
-            </div>
-            <div class=fakeAdvertisement>
-                <div class=fakeAdvertisement-first>
-                </div>
-                <div class=fakeAdvertisement-second>
-                </div>
-            </div>
-        </div>
-        <div class=producerProfile>
-            <div class=producerProfile-bar>
-                <p>管理人</p>
-            </div>
-            <div class=producerProfile-container>
-            </div>
-        </div>
-        <div class=articles>
-            <div class=container-article>
-            <?php foreach($all as $one) { ?>
-                <div class=article>
-                    <div class=text-box>
-                        <div class=genreImage>
-                        </div>
-                        <div class=article-title>
-                            <a href="article.php?article_id=<?php echo $one[0]//ID ?>"><?php echo $one[2]//タイトル; ?></a>
-                        </div>
-                        <div class=article-wrap>
-                            <?
-                                echo $one[3]; // 内容
-                            ?>
-                        </div>
-                        <div class=endTime>
-                            <div class=endTime-box>
-                            </div>
-                        </div>
-                        <span class="good btn"> <?php?>いいね</span>
+        <!--ここからメイン-->
+        <div class = "main">
+            <div class = "article-container">
+                <!--ここからメインボックス-->
+                <div class = "main-box">
+                    <!--記事ボックス-->
+                    <div class = "article-box">
                     </div>
                 </div>
-            <?php } ?>
+                <!--ここから記事欄-->
+                <div class = "articles">
+                    <!--ここから記事バー-->
+                    <div class = "article-bar">
+                        <p>＜人気記事まとめ＞</p>
+                    </div>
+                    <!--ここから記事-->
+                    <div class = "article-list">
+                        <?php foreach($all as $one) { ?>
+                        <div class = "article">
+                            <div class = "article-title">
+                                <a href="article.php?article_id=<?php echo $one[0]//ID ?>"><?php echo $one[2]//タイトル; ?></a>
+                            </div>
+                            <div class = "article-content">
+                                <a href="article.php?article_id=<?php echo $one[0]//ID ?>"><?php echo $one[3]; // 内容?></a>
+                            </div>
+                            <div class = "genreImage">
+                            </div>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+                <!--ここから偽広告などなど-->
+                <div class = "sideBar">
+                    <div class = "tagFirst-box"> 
+                        <div class = "tagFirst"> 
+                            <p>＜PR＞</p> 
+                        </div>
+                        <div class = "fakeAdvertisement-first">
+                        </div>
+                    </div>
+                    <div class = "tagSecond-box">
+                        <div class = "tagSecond"> 
+                            <p>＜PR＞</p> 
+                        </div> 
+                        <div class = "fakeAdvertisement-second"></div>
+                    </div>
+                    <div class = "profile-box">
+                        <div class = "profile-bar"> 
+                            <p>＜管理人＞</p> 
+                        </div>
+                        <div class = "profile"></div>
+                    </div>
+                </div>
+                <!--ここから投稿ボタン-->
+                <div class = "post-box">
+                    <div class = "post-btn"> <a href="post.php">投稿ページへ</a> </div>
+                </div>
             </div>
         </div>
-       	<form action="add.php" method="post">
-            <div class=post>
-                <div class=post-logo>
-                    <p>投稿</p>
-                </div>
-                <div class=post-title>
-                    <p>タイトル</p>
-                    <input type="text" name="title" maxlength="20" placeholder="タイトル">
-                </div>
-                <div class=post-article>
-                    <p>記事</p>
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method ="post">
-                        <textarea name="article_content" rows="4"cols="40" placeholder="ここに記事をお書きください"></textarea>
-                        <input type="submit" name="send" href="article.php" value="投稿">
-                    </form>
-                </div>
-            </div>
-        </form>
+        <!--ここからフッター-->
         <footer>
         </footer>
-        <p id="pageTop"><a href="#"><i class="fa fa-chevron-up"></i></a></p>        
+        <!--スクロールボタン-->
+        <p id = "pageTop"><a href = "#"><i class = "fa fa-chevron-up"></i></a></p>
         <!--js読み込み-->
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="assets/js/pagetop.js" type="text/javascript"></script>
+        <!--jQuery読み込み-->
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src = "assets/js/pagetop.js" type = "text/javascript"></script>
     </body>
 </html>
